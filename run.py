367c0c654ad3c21fc0e9332f6661adb9e9d3ce1b
Savy92 <>
2023-07-12 19:50:39 +0000

    Create polute_ships function to generate ships to use for both player & user board

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ board = []
 for i in range(BOARD_SIZE):
     board.append([HIDDEN]* BOARD_SIZE)
 
+
 def print_board(board):
     """
     Generates the starting board.
@@ -19,4 +20,19 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 
-print_board(board)
+
+def polute_ships(board):
+    """
+    Random placement of ships allocated to board.
+    """
+    for ship in range(NUM_SHIPS):
+        ship_row, ship_col = randint(0, BOARD_SIZE), randint(0, BOARD_SIZE)
+        while board[ship_row][ship_col] == SHIPS
+        ship_row, ship_col = randint(0, BOARD_SIZE), randint(0, BOARD_SIZE)
+        board[ship_row][ship_col] = SHIPS
+
+
+
+
+
+
