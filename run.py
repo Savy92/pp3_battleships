9535550740ab1b5f12ac23d0bb63ec381c7b1b23
Savy92 <>
2023-07-12 19:16:34 +0000

    Initial commit, create print_board function to create & print board

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+#Constants
+BOARD_SIZE = 5
+NUM_SHIPS = 5
+HIDDEN = "O"
+SHIPS = "S"
+GUESS = "X"
+
+#Create board
+board = []
+for i in range(BOARD_SIZE):
+    board.append([HIDDEN]* BOARD_SIZE)
+
+def print_board(board):
+    """
+    Generates the starting board.
+    """
+    for row in board:
+        print(" ".join(row))
+
+print_board(board)
